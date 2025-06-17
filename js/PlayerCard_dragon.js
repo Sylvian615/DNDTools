dfe9716fdb2de2c47da8282f20161548bc57ba1e
sylvian <sylvian.y@outlook.com>
2025-06-18 01:01:40 +0800

    拆分js脚本&新增角色卡页面

---
+++ js/PlayerCard_dragon.js
@@ -0,0 +1,12 @@
+function PlayerCard() {
+    return {
+        charName:"希尔维安",
+        charRace:"精灵-日精灵",
+        charAge:"约100岁",
+        charAlignment:"混乱善良",
+        charJob:"5法师/4观星者/5银月城法术守卫",
+        charAttName:['力量','敏捷','体质','智力','感知','魅力'],
+        charAttValue:[10,16,14,20,12,8],
+        charLanguage:['通用语','精灵语','地底通用语','龙语','深渊语','深渊语','深渊语','深渊语','深渊语','深渊语','深渊语']
+    }
+}
